c9e256ad4d4c279663a595ee296432e862ae9c8a
Attila Olah <NoNameProvided@users.noreply.github.com>
2019-05-17 14:45:31 +0200

    refactor: return user instead of username in ProfileService

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Member } from '../interfaces';
+import { uuid } from '../utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
+  private readonly memeberId: string = uuid();
   private readonly USER_NAME_KEY = 'sm.config.username';
 
   get userNameSet(): boolean {
@@ -13,11 +16,18 @@ export class ProfileService {
     return typeof name === 'string' && name.length > 0;
   }
 
-  get userName(): string {
-    return localStorage.getItem(this.USER_NAME_KEY);
-  }
-
   set userName(value: string) {
     localStorage.setItem(this.USER_NAME_KEY, value);
+  }
+
+  get user(): Member {
+    if (this.userNameSet) {
+      return {
+        id: this.memeberId,
+        name: localStorage.getItem(this.USER_NAME_KEY)
+      };
+    }
+
+    return undefined;
   }
 }
