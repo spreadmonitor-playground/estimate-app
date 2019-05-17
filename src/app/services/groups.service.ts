f2606b42b338bcbf538132970dfba10ebe7f62d3
Albert Csatári <albert.csatari@spreadmonitor.com>
2019-05-17 14:29:07 +0200

    feat: add dummy socket io connection

--- src/app/services/groups.service.ts
+++ src/app/services/groups.service.ts
@@ -1,3 +1,4 @@
+import * as io from 'socket.io-client';
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { Group } from '../interfaces';
@@ -7,12 +8,32 @@ import { Group } from '../interfaces';
 })
 export class GroupsService {
 
+    private url = 'http://192.168.100.106:8080';
+
     public groups: Observable<Group[]> = new Subject();
 
     public selectedGroup: Observable<Group> = new Subject();
 
     public enterGroup(id: string) {
+        const socket = io(this.url);
 
+        socket.on('connect', (e) => {
+            console.log('sziomio', e);
+            console.log(e);
+        });
+
+        socket.on('event', data => {
+            console.log('jonevalami', data);
+        });
+
+        socket.on('hello', vmi => {
+            console.log('jon', vmi);
+            socket.emit('test', { jozsi: 'kecske' });
+        });
+
+        socket.on('disconnect', (a) => {
+            console.log('ohshit', a);
+        });
     }
 
     public leaveGroup(id: string) {
