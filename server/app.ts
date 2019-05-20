55c4e36b0e47425d43d479b1b499ecad63f3d0ae
Marton Horvath <marton.horvath@spreadmonitor.com>
2019-05-20 20:19:38 +0200

    feat: use typescript in the main app

--- server/app.ts
+++ server/app.ts
@@ -1,41 +1,56 @@
-import { Socket } from 'socket.io';
-import { cors } from './cors';
-
-const app = require("express")();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+import * as express from "express";
+import { Server } from "http";
+import * as socketIo from "socket.io";
+import { Socket } from "socket.io";
+import { cors } from "./cors";
 
 const {
   sendGroups,
   createGroup,
   enterGroup,
   leaveGroup
-} = require('./group.handlers');
-const { startEstimation } = require('./estimation.handler');
+} = require("./group.handlers");
+const { startEstimation } = require("./estimation.handler");
 
-app.use(cors);
+function main(): void {
 
-server.listen(8080, "0.0.0.0");
+  const app = express();
+  const server = new Server(app);
+  const io = socketIo(server);
 
-io.sockets.on("connection", (socket: Socket) => {
-  handleConnect(socket);
+  app.use(cors);
 
-  socket.on("createGroup", createGroup(socket));
+  server.listen(8080, "0.0.0.0");
 
-  socket.on("enterGroup", enterGroup(socket));
+  bindSocketEvents(io);
 
-  socket.on('startEstimation', startEstimation(socket))
+}
 
-  socket.on("leaveGroup", leaveGroup(socket));
+function bindSocketEvents(io: socketIo.Server): void {
 
-  socket.on("disconnect", handleDisconnect);
-});
+  io.sockets.on("connection", (socket: Socket) => {
+    handleConnect(socket);
+  
+    socket.on("createGroup", createGroup(socket));
+  
+    socket.on("enterGroup", enterGroup(socket));
+  
+    socket.on("startEstimation", startEstimation(socket));
+  
+    socket.on("leaveGroup", leaveGroup(socket));
+  
+    socket.on("disconnect", handleDisconnect);
+  });
 
-const handleConnect = socket => {
+}
+
+function handleConnect(socket: Socket): void {
   console.log("A user has been connected!");
   sendGroups(socket);
-};
+}
 
-const handleDisconnect = () => {
+function handleDisconnect(socket: Socket): void {
   console.log("A user has been disconnected!");
-};
+}
+
+main();
